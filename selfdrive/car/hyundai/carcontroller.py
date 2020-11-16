e726cf4410291c583c67a68e61094914a0dc92d2
yunbong2 <49453243+yunbong2@users.noreply.github.com>
2020-11-16 22:48:05 +0900

    Update carcontroller.py

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -217,7 +217,7 @@ class CarController():
     if CS.out.leftBlinker and CS.out.rightBlinker:
       self.emergency_manual_timer = 10
     if abs(CS.out.steeringTorque) > 200:
-      self.driver_steering_torque_above_timer = 100
+      self.driver_steering_torque_above_timer = 10
     if self.lanechange_manual_timer:
       lkas_active = 0
     if self.lanechange_manual_timer > 0:
